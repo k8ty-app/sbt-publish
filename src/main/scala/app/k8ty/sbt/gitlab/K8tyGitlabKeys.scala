c6db72fb3d08569d24ec11f8752e0f2307953a25
Mark Rudolph <mark@k8ty.app>
2020-06-24 20:38:29 -0400

    another gitlab naming refactor

--- src/main/scala/app/k8ty/sbt/gitlab/K8tyGitlabKeys.scala
+++ src/main/scala/app/k8ty/sbt/gitlab/K8tyGitlabKeys.scala
@@ -9,8 +9,8 @@ trait K8tyGitlabKeys {
   lazy val configureTokenDispatcher = taskKey[Unit]("Configures a URLHandlerDispatcher for use with a Personal Access Token (GL_PAC_Token)")
   lazy val configureCIDispatcher = taskKey[Unit]("Configures a URLHandlerDispatcher for use with a Personal Access Token (CI_JOB_TOKEN)")
 
-  lazy val gitlabCIPublish = taskKey[Unit]("Publish a projects artifacts to it's GitLab Maven endpoint via a CI Job (CI_JOB_TOKEN)")
-  lazy val gitlabTokenPublish = taskKey[Unit]("Publish a projects artifacts to it's GitLab Maven endpoint via a Personal Access Token (GL_PAC_Token)")
+  lazy val k8tyGitlabCIPublish = taskKey[Unit]("Publish a projects artifacts to it's GitLab Maven endpoint via a CI Job (CI_JOB_TOKEN)")
+  lazy val k8tyGitlabTokenPublish = taskKey[Unit]("Publish a projects artifacts to it's GitLab Maven endpoint via a Personal Access Token (GL_PAC_Token)")
 
 
 }
